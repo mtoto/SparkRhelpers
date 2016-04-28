0923ba8f282124820ef5fe003f9873f9b3d72ab9
Tamas <Transavia@Tamass-MacBook-Pro.local>
2016-04-28 10:39:33 +0200

    Added new histogram function

---
+++ R/hist.sprk.R
@@ -0,0 +1,19 @@
+#' A Histogram Function
+#'
+#' This function generates a frequency histogram
+#' of single columns from a SparkR DataFrame.
+#' @param df The name of the DataFrame.
+#' @param col The name of the column, character string.
+#' @keywords data-exploration, table, frequency, visualisation
+#' @export
+#' @examples
+#' hist.sprk(iris, "Sepal_Length")
+
+hist.sprk <- function(df, col) {
+  tab.sorted <- tab(df, col)
+  graph <- hist(tab.sorted,
+                xlab = col,
+                main = paste("Histogram of" , col))
+  return(graph)
+}
+
