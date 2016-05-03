0c841b6882b7b51fff25bf1026260474c9155020
Tamas <tszilagyi@outlook.com>
2016-05-03 13:12:14 +0200

    cleaned up commandline githb messuup

--- R/median.sR.R
+++ R/median.sR.R
@@ -1,12 +1,9 @@
 #' A Median Function
 #'
-<<<<<<< HEAD
 #' This function computes the sample median of a
 #' single column from a SparkR DataFrame.
-=======
 #' This function computes the sample median
 #' of a single column from a SparkR DataFrame.
->>>>>>> median
 #' @param df The name of the DataFrame.
 #' @param col The name of the column, character string.
 #' @keywords data-exploration, median, frequency, sparkR, summary statistics
@@ -18,15 +15,7 @@ median.sR <- function(df, col) {
 
   tab.sorted <- table.sR(df, col)
   index <- min(which(cumsum(tab.sorted) >= sum(tab.sorted)/2))
-<<<<<<< HEAD
-<<<<<<< HEAD
-  med <- as.numeric(names(t)[index])
-=======
   med <- as.numeric(names(tab.sorted)[index])
->>>>>>> median
-=======
-  med <- as.numeric(names(tab.sorted)[index])
->>>>>>> mode
   return(med)
 
 }
